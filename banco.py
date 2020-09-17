acc32db77e1184cb86661a6dd5a8cad670274a64
rprojas <70291111+rprojas@users.noreply.github.com>
2020-09-16 23:04:43 -0500

    Update banco.py

--- banco.py
+++ banco.py
@@ -19,3 +19,20 @@ class Cliente:
 	def imprimir(self):
 		print(self.nombre, " tiene depositada una cantidad de ",self.cantidad )
 		print("")
+
+		
+		class Banco:
+
+	def __init__(self):
+		self.cliente1=Cliente(input("Nombre cliente 1: \n"))
+		self.cliente2=Cliente(input("Nombre cliente 2: \n"))
+		self.cliente3=Cliente(input("Nombre cliente 3: \n"))
+
+
+	def operacion(self):
+		self.cliente2.depositar(1000)
+		self.cliente3.depositar(2000)
+		self.cliente1.extraer(300)
+		self.cliente1.depositar(5000)
+		self.cliente3.depositar(5000)
+		self.cliente2.extraer(500)
